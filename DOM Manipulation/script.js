ab22b44980e3d50b02a5981bb00900ac527a0c11
Rayjan <rjtamo@gmail.com>
2022-10-19 12:30:27 -0800

    DOM events exercise
    Currently working on item 142- Exercise: DOM Events

---
+++ DOM Manipulation/script.js
@@ -0,0 +1,44 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength()
+{
+	return input.value.length;
+};
+
+function createListElement()
+{
+	var li = document.createElement("li");
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);	
+		input.value = "";
+};
+
+function doneToggle()
+{
+	if (li.done === true)	
+	{
+		li.done === false
+	}
+	else if (li.done === false)
+	{
+		li.done === true
+	};
+};
+
+var addListItem = function(event)
+{
+	if (input.value.length > 0 && event.button === 0) 
+	{
+		createListElement();
+	}
+	else if (input.value.length > 0 && event.keyCode === 13) 
+	{
+		createListElement();
+	};
+};
+
+button.addEventListener("click", addListItem);
+
+input.addEventListener("keypress", addListItem);
